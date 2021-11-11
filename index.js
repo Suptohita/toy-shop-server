ab9249887279ba0be718356f8b4144cc2477d35e
Arvind <arbindudebnath59985@gmail.com>
2021-11-11 12:16:08 +0600

    .

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-hello world
+const express = require('express')
+const cors = require('cors')
+const {MongoClient} = require('mongodb')
+
+const app = express()
+const port = process.env.PORT || 8000
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+// testing 
+app.get('/', (req, res) =>{
+    res.send('Server Running...')
+})
+
+
+// Listening Port 
+app.listen(port, () => {
+    console.log("Server is Running at", port)
+})
