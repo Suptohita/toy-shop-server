a5c1599b3471d3b9319a1d123fd7045052d19720
Arvind <arbindudebnath59985@gmail.com>
2021-11-11 15:54:31 +0600

    load data product data from database

--- index.js
+++ index.js
@@ -5,9 +5,37 @@ const {MongoClient} = require('mongodb')
 const app = express()
 const port = process.env.PORT || 8000
 
+
 // middleware 
 app.use(cors())
 app.use(express.json())
+
+const uri = "mongodb+srv://toy-shop:aUsbB2fXGWfFgldH@cluster0.tv1bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try{
+        await client.connect()
+        
+        const database = client.db('toy-shop')
+        const productsCollection = database.collection('products-collection')
+
+        // get all products 
+        app.get('/allProducts', async(req, res) => {
+            const cursor = productsCollection.find({})
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+
+
+    }
+    finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
 
 
 // testing 
