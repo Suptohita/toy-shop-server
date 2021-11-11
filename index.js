865aabd369e92c5a6568c7870fc023ff0ea94d68
Arvind <arbindudebnath59985@gmail.com>
2021-11-11 16:16:50 +0600

    added env file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const {MongoClient} = require('mongodb')
 
+require('dotenv').config()
+
 const app = express()
 const port = process.env.PORT || 8000
 
@@ -10,8 +12,9 @@ const port = process.env.PORT || 8000
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://toy-shop:aUsbB2fXGWfFgldH@cluster0.tv1bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tv1bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 async function run() {
